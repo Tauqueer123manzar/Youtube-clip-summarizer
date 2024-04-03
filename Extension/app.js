ff96e2f798800aeeb2b3eba780eba8d0b4b59592
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-04-04 04:18:42 +0530

    second commit

--- Extension/app.js
+++ Extension/app.js
@@ -1,18 +1,20 @@
-let btn = document.getElementById("Summarise");
-btn.addEventListener("click", function() {
-    btn.disabled = true;
-    btn.innerHTML = "Summarising...";
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-        var url = tabs[0].url;
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://127.0.0.1:5000/summary?url=" + url, true);
-        xhr.onload = function() {
-            var text = xhr.responseText;
-            const p = document.getElementById("output");
-            p.innerHTML = text;
-            btn.disabled = false;
-            btn.innerHTML = "Summarise";
-        }
-        xhr.send();
+document.addEventListener("DOMContentLoaded", function() {
+    var btn = document.getElementById("Summarise");
+    btn.addEventListener("click", function() {
+        btn.disabled = true;
+        btn.innerHTML = "Summarising...";
+        chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+            var url = tabs[0].url;
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "http://127.0.0.1:5000/summary?url=" + encodeURIComponent(url), true);
+            xhr.onload = function() {
+                var text = xhr.responseText;
+                const p = document.getElementById("output");
+                p.innerHTML = text;
+                btn.disabled = false;
+                btn.innerHTML = "Summarise";
+            }
+            xhr.send();
+        });
     });
 });
--- app.py
+++ app.py
@@ -22,10 +22,7 @@ def get_summary(transcript):
     for i in range(0, (len(transcript)//1000)+1):
         summary_text = summariser(transcript[i*1000:(i+1)*1000])[0]['summary_text']
         summary = summary + summary_text + ' '
-    return summary
-    
+    return summary  
 
 if __name__ == '__main__':
     app.run()
-
-
