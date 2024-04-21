09eba1873b547de1246b56a33e807a2ed5fb3e1b
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-04-21 22:56:31 +0530

    Final code

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask, request
 from youtube_transcript_api import YouTubeTranscriptApi
-from transformers import pipeline
+from transformers import pipeline,AutoTokenizer, AutoModelForSeq2SeqLM
 
 app = Flask(__name__)
+
+# Specify the model and revision explicitly
+model_name = "sshleifer/distilbart-cnn-12-6"
+revision = "a4f8f3e"
 
 @app.get('/summary')
 def summary_api():
     url = request.args.get('url', '')
     video_id = url.split('=')[1]
-    summary = get_summary(get_transcript(video_id))
-    return summary, 200
+    transcript = get_transcript(video_id)
+    if transcript:
+        summary = get_summary(transcript)
+        return summary, 200
+    else:
+        return "No subtitles found for the provided video URL.", 404
 
 def get_transcript(video_id):
     transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
@@ -17,13 +25,18 @@ def get_transcript(video_id):
     return transcript
 
 def get_summary(transcript):
-    summariser = pipeline('summarization')
+    # Load the model and tokenizer
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name, revision=revision)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, revision=revision)
+    
+    summariser = pipeline('summarization', model=model, tokenizer=tokenizer)
     summary = ''
+    input_length = len(transcript)
+    max_length = min(200, input_length)  # Adjust the maximum length dynamically
     for i in range(0, (len(transcript)//1000)+1):
-        summary_text = summariser(transcript[i*1000:(i+1)*1000])[0]['summary_text']
+        summary_text = summariser(transcript[i*1000:(i+1)*1000], max_length=max_length)[0]['summary_text']
         summary = summary + summary_text + ' '
-    return summary 
-
+    return summary
 
 if __name__ == '__main__':
     app.run()
